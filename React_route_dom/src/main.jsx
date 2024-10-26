a6bda9723b6d6b956d1b30f79743bad4894f5f48
vikrant48 <vikrantchauhan9794@gmail.com>
2024-10-26 16:49:23 +0530

    first commit

---
+++ React_route_dom/src/main.jsx
@@ -0,0 +1,53 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import Contect from './Components/Contect/Contect.jsx'
+import User from './Components/User/User.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home/>
+//       },
+//       {
+//         path: 'about',
+//         element: <About/>
+//       },
+//       {
+//         path: 'contect',
+//         element: <Contect/>
+//       }
+//     ]
+//   }
+
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element= {<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contect' element={<Contect/>}/>
+      <Route path='user/:userid' element={<User/>}/>
+      
+
+    </Route>
+    
+    
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
